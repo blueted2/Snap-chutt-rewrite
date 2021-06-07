e4ae06cbe4ac917a06baa8ee4e8d40b657e31307
Gregory Holder <gregory.holder76@gmail.com>
2021-06-07 14:38:23 +0200

    Typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This is the main entrypoint for the program. It will create a database, give it the file from which it will load save save, then it will start the main menu.
+This is the main entrypoint for the program. It will create a database, give it the file from which it will load and save, then it will start the main menu.
 """
 
 from menus import main_menu
